tkiel/rails

commit 65843e1acc0c8d285ff79f8c9c49d4d1215440be

Represent association scope options as AR::Relations insternally.

activerecord/lib/active_record/associations/association.rb
@@ -82,7 +82,7 @@ module ActiveRecord
       end
 
       def scoped
-        target_scope.merge(association_scope).merge(reflection_scope)
+        target_scope.merge(association_scope)
       end
 
       # The scope for this association.
@@ -99,10 +99,6 @@ module ActiveRecord
 
       def reset_scope
         @association_scope = nil
-      end
-
-      def reflection_scope
-        reflection.scope && klass.instance_exec(&reflection.scope)
       end
 
       # Set the inverse association, if possible
@@ -150,6 +146,21 @@ module ActiveRecord
         else
           sql
         end
+      end
+
+      # We can't dump @reflection since it contains the scope proc
+      def marshal_dump
+        reflection  = @reflection
+        @reflection = nil
+
+        ivars = instance_variables.map { |name| [name, instance_variable_get(name)] }
+        [reflection.name, ivars]
+      end
+
+      def marshal_load(data)
+        reflection_name, ivars = data
+        ivars.each { |name, val| instance_variable_set(name, val) }
+        @reflection = @owner.class.reflect_on_association(reflection_name)
       end
 
       private

activerecord/lib/active_record/associations/builder/association.rb
@@ -22,6 +22,49 @@ module ActiveRecord::Associations::Builder
       else
         @scope   = nil
         @options = scope
+
+        convert_deprecated_options_to_scope!
+      end
+    end
+
+    # FIXME: references should not be in this list
+    DEPRECATED_OPTIONS = [:readonly, :references, :order, :limit, :joins, :group, :having,
+                          :offset, :select, :uniq, :include, :conditions]
+
+    class DeprecatedOptionsProc
+      attr_reader :options
+
+      def initialize(options)
+        @options = options
+      end
+
+      def to_proc
+        options = self.options
+        proc do |owner|
+          if options[:where].respond_to?(:to_proc)
+            context = owner || self
+            where(context.instance_eval(&options[:where]))
+              .merge!(options.except(:where))
+          else
+            scoped(options)
+          end
+        end
+      end
+
+      def arity
+        1
+      end
+    end
+
+    def convert_deprecated_options_to_scope!
+      deprecated_options = options.slice(*DEPRECATED_OPTIONS)
+
+      unless deprecated_options.empty?
+        deprecated_options[:includes] = deprecated_options.delete(:include)    if deprecated_options[:include]
+        deprecated_options[:where]    = deprecated_options.delete(:conditions) if deprecated_options[:conditions]
+
+        @scope   = DeprecatedOptionsProc.new(deprecated_options)
+        @options = options.except(*DEPRECATED_OPTIONS)
       end
     end
 

activerecord/lib/active_record/associations/preloader/association.rb
@@ -2,16 +2,16 @@ module ActiveRecord
   module Associations
     class Preloader
       class Association #:nodoc:
-        attr_reader :owners, :reflection, :preload_options, :model, :klass
+        attr_reader :owners, :reflection, :preload_scope, :model, :klass
 
-        def initialize(klass, owners, reflection, preload_options)
-          @klass           = klass
-          @owners          = owners
-          @reflection      = reflection
-          @preload_options = preload_options || {}
-          @model           = owners.first && owners.first.class
-          @scoped          = nil
-          @owners_by_key   = nil
+        def initialize(klass, owners, reflection, preload_scope)
+          @klass         = klass
+          @owners        = owners
+          @reflection    = reflection
+          @preload_scope = preload_scope
+          @model         = owners.first && owners.first.class
+          @scoped        = nil
+          @owners_by_key = nil
         end
 
         def run
@@ -92,33 +92,28 @@ module ActiveRecord
           records_by_owner
         end
 
+        def reflection_scope
+          @reflection_scope ||= reflection.scope ? klass.unscoped.instance_exec(&reflection.scope) : klass.unscoped
+        end
+
         def build_scope
           scope = klass.unscoped
           scope.default_scoped = true
 
-          scope = scope.where(interpolate(options[:conditions]))
-          scope = scope.where(interpolate(preload_options[:conditions]))
+          values         = reflection_scope.values
+          preload_values = preload_scope.values
 
-          scope = scope.select(preload_options[:select] || options[:select] || table[Arel.star])
-          scope = scope.includes(preload_options[:include] || options[:include])
+          scope.where_values      = Array(values[:where])      + Array(preload_values[:where])
+          scope.references_values = Array(values[:references]) + Array(preload_values[:references])
+
+          scope.select!   preload_values[:select] || values[:select] || table[Arel.star]
+          scope.includes! preload_values[:includes] || values[:includes]
 
           if options[:as]
-            scope = scope.where(
-              klass.table_name => {
-                reflection.type => model.base_class.sti_name
-              }
-            )
+            scope.where!(klass.table_name => { reflection.type => model.base_class.sti_name })
           end
 
           scope
-        end
-
-        def interpolate(conditions)
-          if conditions.respond_to?(:to_proc)
-            klass.send(:instance_eval, &conditions)
-          else
-            conditions
-          end
         end
       end
     end

activerecord/lib/active_record/associations/association_scope.rb
@@ -6,7 +6,7 @@ module ActiveRecord
       attr_reader :association, :alias_tracker
 
       delegate :klass, :owner, :reflection, :interpolate, :to => :association
-      delegate :chain, :conditions, :options, :source_options, :active_record, :to => :reflection
+      delegate :chain, :scope_chain, :options, :source_options, :active_record, :to => :reflection
 
       def initialize(association)
         @association   = association
@@ -15,19 +15,8 @@ module ActiveRecord
 
       def scope
         scope = klass.unscoped
-
         scope.extending!(*Array(options[:extend]))
-
-        # It's okay to just apply all these like this. The options will only be present if the
-        # association supports that option; this is enforced by the association builder.
-        scope.merge!(options.slice(
-          :readonly, :references, :order, :limit, :joins, :group, :having, :offset, :select, :uniq))
-
-        if options[:include]
-          scope.includes! options[:include]
-        elsif options[:through]
-          scope.includes! source_options[:include]
-        end
+        scope.merge! eval_scope(klass, reflection.scope) if reflection.scope
 
         add_constraints(scope)
       end
@@ -82,8 +71,6 @@ module ActiveRecord
             foreign_key = reflection.active_record_primary_key
           end
 
-          conditions = self.conditions[i]
-
           if reflection == chain.last
             bind_val = bind scope, table.table_name, key.to_s, owner[foreign_key]
             scope    = scope.where(table[key].eq(bind_val))
@@ -92,14 +79,6 @@ module ActiveRecord
               value    = owner.class.base_class.name
               bind_val = bind scope, table.table_name, reflection.type.to_s, value
               scope    = scope.where(table[reflection.type].eq(bind_val))
-            end
-
-            conditions.each do |condition|
-              if options[:through] && condition.is_a?(Hash)
-                condition = disambiguate_condition(table, condition)
-              end
-
-              scope = scope.where(interpolate(condition))
             end
           else
             constraint = table[key].eq(foreign_table[foreign_key])
@@ -110,13 +89,15 @@ module ActiveRecord
             end
 
             scope = scope.joins(join(foreign_table, constraint))
+          end
 
-            conditions.each do |condition|
-              condition = interpolate(condition)
-              condition = disambiguate_condition(table, condition) unless i == 0
+          # Exclude the scope of the association itself, because that
+          # was already merged in the #scope method.
+          (scope_chain[i] - [self.reflection.scope]).each do |scope_chain_item|
+            item = eval_scope(reflection.klass, scope_chain_item)
 
-              scope = scope.where(condition)
-            end
+            scope.includes! item.includes_values
+            scope.where_values += item.where_values
           end
         end
 
@@ -138,19 +119,13 @@ module ActiveRecord
         end
       end
 
-      def disambiguate_condition(table, condition)
-        if condition.is_a?(Hash)
-          Hash[
-            condition.map do |k, v|
-              if v.is_a?(Hash)
-                [k, v]
-              else
-                [table.table_alias || table.name, { k => v }]
-              end
-            end
-          ]
+      def eval_scope(klass, scope)
+        return scope if scope.is_a?(Relation)
+
+        if scope.arity == 0
+          klass.unscoped.instance_exec(&scope)
         else
-          condition
+          klass.unscoped.instance_exec(owner, &scope)
         end
       end
     end

activerecord/lib/active_record/associations/collection_association.rb
@@ -183,7 +183,7 @@ module ActiveRecord
 
           reflection.klass.count_by_sql(custom_counter_sql)
         else
-          if options[:uniq]
+          if association_scope.uniq_value
             # This is needed because 'SELECT count(DISTINCT *)..' is not valid SQL.
             column_name ||= reflection.klass.primary_key
             count_options[:distinct] = true
@@ -246,14 +246,14 @@ module ActiveRecord
       # +count_records+, which is a method descendants have to provide.
       def size
         if !find_target? || loaded?
-          if options[:uniq]
+          if association_scope.uniq_value
             target.uniq.size
           else
             target.size
           end
-        elsif !loaded? && options[:group]
+        elsif !loaded? && !association_scope.group_values.empty?
           load_target.size
-        elsif !loaded? && !options[:uniq] && target.is_a?(Array)
+        elsif !loaded? && !association_scope.uniq_value && target.is_a?(Array)
           unsaved_records = target.select { |r| r.new_record? }
           unsaved_records.size + count_records
         else
@@ -344,7 +344,7 @@ module ActiveRecord
         callback(:before_add, record)
         yield(record) if block_given?
 
-        if options[:uniq] && index = @target.index(record)
+        if association_scope.uniq_value && index = @target.index(record)
           @target[index] = record
         else
           @target << record

activerecord/lib/active_record/associations/has_many_association.rb
@@ -46,7 +46,7 @@ module ActiveRecord
           # documented side-effect of the method that may avoid an extra SELECT.
           @target ||= [] and loaded! if count == 0
 
-          [options[:limit], count].compact.min
+          [association_scope.limit_value, count].compact.min
         end
 
         def has_cached_counter?(reflection = reflection)

activerecord/lib/active_record/associations/join_dependency/join_association.rb
@@ -92,14 +92,21 @@ module ActiveRecord
 
             constraint = build_constraint(reflection, table, key, foreign_table, foreign_key)
 
-            conditions = self.conditions[i].dup
-            conditions << { reflection.type => foreign_klass.base_class.name } if reflection.type
+            scope_chain_items = scope_chain[i]
 
-            conditions.each do |condition|
-              condition = active_record.send(:sanitize_sql, interpolate(condition), table.table_alias || table.name)
-              condition = Arel.sql(condition) unless condition.is_a?(Arel::Node)
+            if reflection.type
+              scope_chain_items += [
+                ActiveRecord::Relation.new(reflection.klass, table)
+                  .where(reflection.type => foreign_klass.base_class.name)
+              ]
+            end
 
-              constraint = constraint.and(condition)
+            scope_chain_items.each do |item|
+              unless item.is_a?(Relation)
+                item = ActiveRecord::Relation.new(reflection.klass, table).instance_exec(self, &item)
+              end
+
+              constraint = constraint.and(item.arel.constraints) unless item.arel.constraints.empty?
             end
 
             relation.from(join(table, constraint))
@@ -137,18 +144,8 @@ module ActiveRecord
           table.table_alias || table.name
         end
 
-        def conditions
-          @conditions ||= reflection.conditions.reverse
-        end
-
-        private
-
-        def interpolate(conditions)
-          if conditions.respond_to?(:to_proc)
-            instance_eval(&conditions)
-          else
-            conditions
-          end
+        def scope_chain
+          @scope_chain ||= reflection.scope_chain.reverse
         end
 
       end

activerecord/lib/active_record/associations/preloader.rb
@@ -42,7 +42,7 @@ module ActiveRecord
       autoload :HasAndBelongsToMany, 'active_record/associations/preloader/has_and_belongs_to_many'
       autoload :BelongsTo,           'active_record/associations/preloader/belongs_to'
 
-      attr_reader :records, :associations, :options, :model
+      attr_reader :records, :associations, :preload_scope, :model
 
       # Eager loads the named associations for the given Active Record record(s).
       #
@@ -78,15 +78,10 @@ module ActiveRecord
       #   [ :books, :author ]
       #   { :author => :avatar }
       #   [ :books, { :author => :avatar } ]
-      #
-      # +options+ contains options that will be passed to ActiveRecord::Base#find
-      # (which is called under the hood for preloading records). But it is passed
-      # only one level deep in the +associations+ argument, i.e. it's not passed
-      # to the child associations when +associations+ is a Hash.
-      def initialize(records, associations, options = {})
-        @records      = Array.wrap(records).compact.uniq
-        @associations = Array.wrap(associations)
-        @options      = options
+      def initialize(records, associations, preload_scope = nil)
+        @records       = Array.wrap(records).compact.uniq
+        @associations  = Array.wrap(associations)
+        @preload_scope = preload_scope || Relation.new(nil, nil)
       end
 
       def run
@@ -110,7 +105,7 @@ module ActiveRecord
 
       def preload_hash(association)
         association.each do |parent, child|
-          Preloader.new(records, parent, options).run
+          Preloader.new(records, parent, preload_scope).run
           Preloader.new(records.map { |record| record.send(parent) }.flatten, child).run
         end
       end
@@ -125,7 +120,7 @@ module ActiveRecord
       def preload_one(association)
         grouped_records(association).each do |reflection, klasses|
           klasses.each do |klass, records|
-            preloader_for(reflection).new(klass, records, reflection, options).run
+            preloader_for(reflection).new(klass, records, reflection, preload_scope).run
           end
         end
       end

commit 63e0b96663a68e6d37b74396e445d2416452c0cb

Fix destructive side effects from marshaling an association caused by 65843e1acc0c8d285ff79f8c9c49d4d1215440be

activerecord/lib/active_record/associations/association.rb
@@ -154,11 +154,8 @@ module ActiveRecord
 
       # We can't dump @reflection since it contains the scope proc
       def marshal_dump
-        reflection  = @reflection
-        @reflection = nil
-
-        ivars = instance_variables.map { |name| [name, instance_variable_get(name)] }
-        [reflection.name, ivars]
+        ivars = (instance_variables - [:@reflection]).map { |name| [name, instance_variable_get(name)] }
+        [@reflection.name, ivars]
       end
 
       def marshal_load(data)

activerecord/test/cases/associations/extension_test.rb
@@ -40,9 +40,12 @@ class AssociationsExtensionsTest < ActiveRecord::TestCase
     assert_equal projects(:action_controller), david.projects.find_most_recent
 
     marshalled = Marshal.dump(david)
-    david      = Marshal.load(marshalled)
 
-    assert_equal projects(:action_controller), david.projects.find_most_recent
+    # Marshaling an association shouldn't make it unusable by wiping its reflection.
+    assert_not_nil david.association(:projects).reflection
+
+    david_too  = Marshal.load(marshalled)
+    assert_equal projects(:action_controller), david_too.projects.find_most_recent
   end
 
   def test_marshalling_named_extensions