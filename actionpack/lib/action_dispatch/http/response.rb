tkiel/rails

commit 0004ca3a9725b5c89b671d6f5bcfb245a5800af7

More documentation for ActionDispatch::Response
[ci skip]

actionpack/lib/action_dispatch/http/response.rb
@@ -172,7 +172,8 @@ module ActionDispatch # :nodoc:
       stream.to_path
     end
 
-    # Returns the content of the response as a String.
+    # Returns the content of the response as a String. This contains the contents
+    # of any calls to <tt>render</tt>.
     def body
       strings = []
       each { |part| strings << part.to_s }
@@ -181,6 +182,7 @@ module ActionDispatch # :nodoc:
 
     EMPTY = " "
 
+    # Allows you to set manually set or override the response body.
     def body=(body)
       @blank = true if body == EMPTY
 

commit 9b80d7b0dfe2852b818f61057c16de6c592c94f9

Fix typo introduced in 0004ca3a
[ci skip]

actionpack/lib/action_dispatch/http/response.rb
@@ -182,7 +182,7 @@ module ActionDispatch # :nodoc:
 
     EMPTY = " "
 
-    # Allows you to set manually set or override the response body.
+    # Allows you to manually set or override the response body.
     def body=(body)
       @blank = true if body == EMPTY
 