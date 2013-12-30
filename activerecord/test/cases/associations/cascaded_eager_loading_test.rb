tkiel/rails

commit 2075f39d726cef361170218fd16421fc52bed5a8

eliminate the `loaded?` conditional

activerecord/lib/active_record/associations/preloader.rb
@@ -174,14 +174,18 @@ module ActiveRecord
       end
 
       class NullPreloader
-        attr_reader :owners
+        attr_reader :owners, :reflection
 
         def initialize(klass, owners, reflection, preload_scope)
           @owners = owners
+          @reflection = reflection
         end
 
         def run(preloader); end
-        def loaded?; false; end
+
+        def preloaded_records
+          owners.flat_map { |owner| owner.read_attribute reflection.name }
+        end
       end
 
       def preloader_for(reflection, owners)

activerecord/lib/active_record/associations/preloader/association.rb
@@ -17,7 +17,6 @@ module ActiveRecord
           @owners_by_key = nil
           @type_caster   = IDENTITY_CASTER
           @associated_records_by_owner = nil
-          @loaded = false
         end
 
         def run(preloader)
@@ -74,15 +73,9 @@ module ActiveRecord
           @associated_records_by_owner.values.flatten
         end
 
-        def loaded?
-          @loaded
-        end
-
         private
 
         def associated_records_by_owner(preloader)
-          @loaded = true
-
           return @associated_records_by_owner if @associated_records_by_owner
 
           owners_map = owners_by_key

activerecord/lib/active_record/associations/preloader/through_association.rb
@@ -12,8 +12,6 @@ module ActiveRecord
         end
 
         def associated_records_by_owner(preloader)
-          @loaded = true
-
           return @associated_records_by_owner if @associated_records_by_owner
 
           preloader.preload(owners,
@@ -54,7 +52,7 @@ module ActiveRecord
                 r.send(source_reflection.name)
               }.compact
 
-              if pl && pl.loaded?
+              if pl
                 loaded_records = pl.preloaded_records
                 i = 0
                 record_index = loaded_records.each_with_object({}) { |r,indexes|

commit 69853822d72401c11488375b07f27441b82dee73

https://github.com/rails/rails/commit/2075f39d726cef361170218fd16421fc52bed5a8 introduced a regression in includes/preloades by calling `read_attribute` on an association when preloading takes places, instead of using loaded records in `association.target`.
tl;dr

Records are not made properly available via `read_attribute` when preloding in simultaneous,
but value of `@loaded` is already set true, and records concatenated in `association.target` on an association object.
When `@loaded` is true we return an object of `AlreadyLoaded` in preload_for. In `AlreadyLoaded` to return preloaded
records we make wrong use of `read_attribute`, instead of `target` records.

The regression is fixed by making use of the loaded records in `association.target` when the preloading takes place.

Fixes #13437

activerecord/lib/active_record/associations/preloader.rb
@@ -183,7 +183,7 @@ module ActiveRecord
         def run(preloader); end
 
         def preloaded_records
-          owners.flat_map { |owner| owner.read_attribute reflection.name }
+          owners.flat_map { |owner| owner.association(reflection.name).target }
         end
       end
 

activerecord/test/cases/associations/cascaded_eager_loading_test.rb
@@ -174,4 +174,18 @@ class CascadedEagerLoadingTest < ActiveRecord::TestCase
     sink = Vertex.all.merge!(:includes=>{:sources=>{:sources=>{:sources=>:sources}}}, :order => 'vertices.id DESC').first
     assert_equal vertices(:vertex_1), assert_no_queries { sink.sources.first.sources.first.sources.first.sources.first }
   end
+
+  def test_eager_association_loading_with_cascaded_interdependent_one_level_and_two_levels
+    authors_relation = Author.all.merge!(:includes => [:comments, {:posts => :categorizations}], :order => "authors.id")
+    assert_nothing_raised do
+      authors_relation.to_a
+    end
+    authors = authors_relation.to_a
+    assert_equal 3, authors.size
+    assert_equal 10, authors[0].comments.size
+    assert_equal 1, authors[1].comments.size
+    assert_equal 5, authors[0].posts.size
+    assert_equal 3, authors[1].posts.size
+    assert_equal 3, authors[0].posts.collect { |post| post.categorizations.size }.inject(0) { |sum, i| sum+i }
+  end
 end