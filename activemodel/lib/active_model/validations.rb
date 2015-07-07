tkiel/rails

commit dee4fbc90be183fc1596ad9c59d9ba1fbdb16ce7

Don't document private internal constant [ci skip]

activemodel/lib/active_model/validations.rb
@@ -87,6 +87,7 @@ module ActiveModel
         validates_with BlockValidator, _merge_attributes(attr_names), &block
       end
 
+      # :nodoc:
       VALID_OPTIONS_FOR_VALIDATE = [:on, :if, :unless, :prepend].freeze
 
       # Adds a validation method or block to the class. This is useful when

commit 419bda27092b0012e030898b612b87c74016511f

docs, remove accidental :nodoc: of ActiveModel::Validations::ClassMethods methods.
[ci skip]

While this :nodoc: did hide the constant it also removed the following
methods from the API docs:

- #attribute_method?
- #clear_validators!
- #validate
- #validators
- #validators_on

Those are public API and should be visible.

Issue was caused by dee4fbc

/cc @zzak

activemodel/lib/active_model/validations.rb
@@ -87,8 +87,7 @@ module ActiveModel
         validates_with BlockValidator, _merge_attributes(attr_names), &block
       end
 
-      # :nodoc:
-      VALID_OPTIONS_FOR_VALIDATE = [:on, :if, :unless, :prepend].freeze
+      VALID_OPTIONS_FOR_VALIDATE = [:on, :if, :unless, :prepend].freeze # :nodoc:
 
       # Adds a validation method or block to the class. This is useful when
       # overriding the +validate+ instance method becomes too unwieldy and