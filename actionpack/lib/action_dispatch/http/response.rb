tkiel/rails

commit 492b134433653cbbea1b30c459d41054f0da5ad7

Push `before_sending` to super class
We want to get rid of the `Live::Response` so we are consolidating methods
from `Live::Response` and `Response` by merging them together.

This adds an `#empty` method to the request so we don't need to
hard-code the empty array each time we call an empty
`ActionDispatch::Request`.

The work here is a continuation on combining controller and integration
test code bases into one.

actionpack/lib/action_dispatch/http/response.rb
@@ -412,6 +412,8 @@ module ActionDispatch # :nodoc:
     end
 
     def before_sending
+      headers.freeze
+      request.cookie_jar.commit!
     end
 
     def build_buffer(response, body)

actionpack/lib/action_controller/metal/live.rb
@@ -223,12 +223,6 @@ module ActionController
         jar.write self unless committed?
       end
 
-      def before_sending
-        super
-        request.cookie_jar.commit!
-        headers.freeze
-      end
-
       def build_buffer(response, body)
         buf = Live::Buffer.new response
         body.each { |part| buf.write part }

actionpack/lib/action_dispatch/http/request.rb
@@ -49,6 +49,10 @@ module ActionDispatch
       METHOD
     end
 
+    def self.empty
+      new({})
+    end
+
     def initialize(env)
       super
       @method            = nil

actionpack/lib/action_dispatch/testing/integration.rb
@@ -375,6 +375,7 @@ module ActionDispatch
           @request  = ActionDispatch::Request.new(session.last_request.env)
           response = _mock_session.last_response
           @response = ActionDispatch::TestResponse.from_response(response)
+          @response.request = @request
           @html_document = nil
           @url_options = nil
 

actionpack/test/controller/base_test.rb
@@ -93,7 +93,7 @@ end
 class ControllerInstanceTests < ActiveSupport::TestCase
   def setup
     @empty = EmptyController.new
-    @empty.set_request!(ActionDispatch::Request.new({}))
+    @empty.set_request!(ActionDispatch::Request.empty)
     @empty.set_response!(EmptyController.make_response!(@empty.request))
     @contained = Submodule::ContainedEmptyController.new
     @empty_controllers = [@empty, @contained]

actionpack/test/controller/new_base/bare_metal_test.rb
@@ -26,7 +26,7 @@ module BareMetalTest
 
     test "response_body value is wrapped in an array when the value is a String" do
       controller = BareController.new
-      controller.set_request!(ActionDispatch::Request.new({}))
+      controller.set_request!(ActionDispatch::Request.empty)
       controller.set_response!(BareController.make_response!(controller.request))
       controller.index
       assert_equal ["Hello world"], controller.response_body

actionpack/test/dispatch/cookies_test.rb
@@ -7,7 +7,7 @@ class CookieJarTest < ActiveSupport::TestCase
   attr_reader :request
 
   def setup
-    @request = ActionDispatch::Request.new({})
+    @request = ActionDispatch::Request.empty
   end
 
   def test_fetch

actionpack/test/dispatch/live_response_test.rb
@@ -6,7 +6,7 @@ module ActionController
     class ResponseTest < ActiveSupport::TestCase
       def setup
         @response = Live::Response.new
-        @response.request = ActionDispatch::Request.new({}) #yolo
+        @response.request = ActionDispatch::Request.empty
       end
 
       def test_header_merge

commit 27530a6193fa903b3b065f5241eda1fc37538618

Only commit the cookie jar if it hasn't been committed
We don't want to commit the cookie jar more than once because then we
will be attempting to modify a frozen hash.

Fixes Railties test failure caused by 492b134.

actionpack/lib/action_dispatch/http/response.rb
@@ -413,7 +413,7 @@ module ActionDispatch # :nodoc:
 
     def before_sending
       headers.freeze
-      request.commit_cookie_jar!
+      request.commit_cookie_jar! unless committed?
     end
 
     def build_buffer(response, body)