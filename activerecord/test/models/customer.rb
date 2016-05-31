tkiel/rails

commit 36e9be8524ff524ac5ea51dd1fc6ee33cfe4285a

Remove composed_of special case from multi-parameter attributes
As we move towards removing the `klass` method from `column`, the else
clause as it stands right now will become the only case.

activerecord/lib/active_record/aggregations.rb
@@ -244,6 +244,10 @@ module ActiveRecord
         def writer_method(name, class_name, mapping, allow_nil, converter)
           define_method("#{name}=") do |part|
             klass = class_name.constantize
+            if part.is_a?(Hash)
+              part = klass.new(*part.values)
+            end
+
             unless part.is_a?(klass) || converter.nil? || part.nil?
               part = converter.respond_to?(:call) ? converter.call(part) : klass.send(converter, part)
             end

activerecord/lib/active_record/attribute_assignment.rb
@@ -126,8 +126,8 @@ module ActiveRecord
       def read_value
         return if values.values.compact.empty?
 
-        @column = object.class.reflect_on_aggregation(name.to_sym) || object.column_for_attribute(name)
-        klass   = column.klass
+        @column = object.column_for_attribute(name)
+        klass   = column ? column.klass : nil
 
         if klass == Time
           read_time
@@ -186,8 +186,7 @@ module ActiveRecord
         positions    = (1..max_position)
         validate_required_parameters!(positions)
 
-        set_values = values.values_at(*positions)
-        klass.new(*set_values)
+        values.slice(*positions)
       end
 
       # Checks whether some blank date parameter exists. Note that this is different

commit ac49ad60020e6c0d3d576b88ca501dc999569621

Ensure hashes can be passed to attributes using `composed_of`
This behavior was broken by 36e9be85. When the value is assigned
directly, either through mass assignment or directly assigning a hash,
the hash gets passed through to this writer method directly. While this
is intended to handle certain cases, when an explicit converter has been
provided, we should continue to use that instead. The positioning of the
added guard caused the new behavior to override that case.

Fixes #25210

activerecord/lib/active_record/aggregations.rb
@@ -256,13 +256,14 @@ module ActiveRecord
         def writer_method(name, class_name, mapping, allow_nil, converter)
           define_method("#{name}=") do |part|
             klass = class_name.constantize
-            if part.is_a?(Hash)
-              raise ArgumentError unless part.size == part.keys.max
-              part = klass.new(*part.sort.map(&:last))
-            end
 
             unless part.is_a?(klass) || converter.nil? || part.nil?
               part = converter.respond_to?(:call) ? converter.call(part) : klass.send(converter, part)
+            end
+
+            if part.is_a?(Hash)
+              raise ArgumentError unless part.size == part.keys.max
+              part = klass.new(*part.sort.map(&:last))
             end
 
             if part.nil? && allow_nil

activerecord/test/cases/aggregations_test.rb
@@ -138,6 +138,11 @@ class AggregationsTest < ActiveRecord::TestCase
     assert_equal 'Barnoit GUMBLEAU', customers(:barney).fullname.to_s
     assert_kind_of Fullname, customers(:barney).fullname
   end
+
+  def test_assigning_hash_to_custom_converter
+    customers(:barney).fullname = { first: "Barney", last: "Stinson" }
+    assert_equal "Barney STINSON", customers(:barney).name
+  end
 end
 
 class OverridingAggregationsTest < ActiveRecord::TestCase

activerecord/test/models/customer.rb
@@ -64,7 +64,12 @@ class Fullname
 
   def self.parse(str)
     return nil unless str
-    new(*str.to_s.split)
+
+    if str.is_a?(Hash)
+      new(str[:first], str[:last])
+    else
+      new(*str.to_s.split)
+    end
   end
 
   def initialize(first, last = nil)