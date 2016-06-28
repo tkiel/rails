tkiel/rails

commit 97d7dc48d68e4c20c17e315c8f4d3f045ed97f5a

pull `to` up
this way we don't have to insert / delete it from the options hash so
many times.

actionpack/lib/action_dispatch/routing/mapper.rb
@@ -61,7 +61,7 @@ module ActionDispatch
         attr_reader :requirements, :conditions, :defaults
         attr_reader :to, :default_controller, :default_action, :as, :anchor
 
-        def self.build(scope, set, path, as, controller, default_action, options)
+        def self.build(scope, set, path, as, controller, default_action, to, options)
           options = scope[:options].merge(options) if scope[:options]
 
           options.delete :only
@@ -72,15 +72,15 @@ module ActionDispatch
 
           defaults = (scope[:defaults] || {}).dup
 
-          new scope, set, path, defaults, as, controller, default_action, scope[:module], options
+          new scope, set, path, defaults, as, controller, default_action, scope[:module], to, options
         end
 
-        def initialize(scope, set, path, defaults, as, controller, default_action, modyoule, options)
+        def initialize(scope, set, path, defaults, as, controller, default_action, modyoule, to, options)
           @requirements, @conditions = {}, {}
           @defaults = defaults
           @set = set
 
-          @to                 = options.delete :to
+          @to                 = to
           @default_controller = controller
           @default_action     = default_action
           @as                 = as
@@ -1539,29 +1539,30 @@ module ActionDispatch
 
           controller = options.delete(:controller) || @scope[:controller]
           option_path = options.delete :path
+          to = options.delete :to
 
           path_types = paths.group_by(&:class)
           path_types.fetch(String, []).each do |_path|
             route_options = options.dup
-            process_path(route_options, controller, _path, option_path || _path)
+            process_path(route_options, controller, _path, option_path || _path, to)
           end
 
           path_types.fetch(Symbol, []).each do |action|
             route_options = options.dup
-            decomposed_match(action, controller, route_options, option_path)
+            decomposed_match(action, controller, route_options, option_path, to)
           end
 
           self
         end
 
-        def process_path(options, controller, path, option_path)
+        def process_path(options, controller, path, option_path, to)
           path_without_format = path.sub(/\(\.:format\)$/, '')
-          if using_match_shorthand?(path_without_format, options[:to], options[:action])
-            options[:to] ||= path_without_format.gsub(%r{^/}, "").sub(%r{/([^/]*)$}, '#\1')
-            options[:to].tr!("-", "_")
+          if using_match_shorthand?(path_without_format, to, options[:action])
+            to ||= path_without_format.gsub(%r{^/}, "").sub(%r{/([^/]*)$}, '#\1')
+            to.tr!("-", "_")
           end
 
-          decomposed_match(path, controller, options, option_path)
+          decomposed_match(path, controller, options, option_path, to)
         end
 
         def using_match_shorthand?(path, to, action)
@@ -1570,22 +1571,22 @@ module ActionDispatch
           path =~ %r{^/?[-\w]+/[-\w/]+$}
         end
 
-        def decomposed_match(path, controller, options, _path) # :nodoc:
+        def decomposed_match(path, controller, options, _path, to) # :nodoc:
           if on = options.delete(:on)
-            send(on) { decomposed_match(path, controller, options, _path) }
+            send(on) { decomposed_match(path, controller, options, _path, to) }
           else
             case @scope.scope_level
             when :resources
-              nested { decomposed_match(path, controller, options, _path) }
+              nested { decomposed_match(path, controller, options, _path, to) }
             when :resource
-              member { decomposed_match(path, controller, options, _path) }
+              member { decomposed_match(path, controller, options, _path, to) }
             else
-              add_route(path, controller, options, _path)
+              add_route(path, controller, options, _path, to)
             end
           end
         end
 
-        def add_route(action, controller, options, _path) # :nodoc:
+        def add_route(action, controller, options, _path, to) # :nodoc:
           path = path_for_action(action, _path)
           raise ArgumentError, "path is required" if path.blank?
 
@@ -1605,7 +1606,7 @@ module ActionDispatch
                  name_for_action(options.delete(:as), action)
                end
 
-          mapping = Mapping.build(@scope, @set, URI.parser.escape(path), as, controller, default_action, options)
+          mapping = Mapping.build(@scope, @set, URI.parser.escape(path), as, controller, default_action, to, options)
           app, conditions, requirements, defaults, as, anchor = mapping.to_route
           @set.add_route(app, conditions, requirements, defaults, as, anchor)
         end

actionpack/test/dispatch/mapper_test.rb
@@ -42,7 +42,7 @@ module ActionDispatch
 
       def test_mapping_requirements
         options = { :via => :get }
-        m = Mapper::Mapping.build({}, FakeSet.new, '/store/:name(*rest)', nil, 'foo', 'bar', options)
+        m = Mapper::Mapping.build({}, FakeSet.new, '/store/:name(*rest)', nil, 'foo', 'bar', nil, options)
         _, _, requirements, _ = m.to_route
         assert_equal(/.+?/, requirements[:rest])
       end

commit 2a3fecaeaa2b0dc44ab520d688b5b055bee333d4

Fix setting route's to in a scope
Fixes #25488
97d7dc4 introduced a regression that resulted in ArgumentError when to
was in options of the scope and not of particular route.

actionpack/lib/action_dispatch/routing/mapper.rb
@@ -1062,6 +1062,10 @@ module ActionDispatch
           def merge_shallow_scope(parent, child) #:nodoc:
             child ? true : false
           end
+
+          def merge_to_scope(parent, child)
+            child
+          end
       end
 
       # Resource routing allows you to quickly declare all of the common routes
@@ -1582,6 +1586,10 @@ module ActionDispatch
             raise ArgumentError, "Unknown scope #{on.inspect} given to :on"
           end
 
+          if @scope[:to]
+            options[:to] ||= @scope[:to]
+          end
+
           if @scope[:controller] && @scope[:action]
             options[:to] ||= "#{@scope[:controller]}##{@scope[:action]}"
           end
@@ -2021,7 +2029,7 @@ to this:
       class Scope # :nodoc:
         OPTIONS = [:path, :shallow_path, :as, :shallow_prefix, :module,
                    :controller, :action, :path_names, :constraints,
-                   :shallow, :blocks, :defaults, :via, :format, :options]
+                   :shallow, :blocks, :defaults, :via, :format, :options, :to]
 
         RESOURCE_SCOPES = [:resource, :resources]
         RESOURCE_METHOD_SCOPES = [:collection, :member, :new]

actionpack/test/dispatch/mapper_test.rb
@@ -102,6 +102,18 @@ module ActionDispatch
         assert_equal("PUT", fakeset.routes.first.verb)
       end
 
+      def test_to_scope
+        fakeset = FakeSet.new
+        mapper = Mapper.new fakeset
+        mapper.scope(to: "posts#index") do
+          mapper.get :all
+          mapper.post :most
+        end
+
+        assert_equal "posts#index", fakeset.routes.to_a[0].defaults[:to]
+        assert_equal "posts#index", fakeset.routes.to_a[1].defaults[:to]
+      end
+
       def test_map_slash
         fakeset = FakeSet.new
         mapper = Mapper.new fakeset